00020ba23ac41c3b63c7e83cc26536e268c9f1bc
Daniel Silva <dani3l.dxp@gmail.com>
2020-01-26 08:26:41 -0300

    feat: first logic exercise

---
+++ src/53 - Exercicio logica/53-logica#2.js
@@ -0,0 +1,7 @@
+/**
+ * Escrever uma função que receba dois argumentos, largura e altura
+ * de uma imagem e retorne true se a imagem estiver em modo paisagem
+ */
+const image = (width, height) => width > height;
+
+console.log(image(120, 500));
