afdbc626f7c949b88e93c3dc50657163b6b0c087
Daniel Silva <dani3l.dxp@gmail.com>
2020-03-17 14:20:40 -0300

    feat: review de Object

---
+++ src/82 - Object and Prototypes/82-review.js
@@ -0,0 +1,69 @@
+const carro = {
+	modelo: 'New Fiesta',
+	preco: 41000,
+	proprietario: {
+		nome: 'Daniel',
+		idade: 32,
+		endereco: {
+			logradouro: 'Rua RADL',
+			numero: 100,
+			bairro: 'Cidade Miguel Badra',
+			cidade: 'Suzano',
+			estado: 'SP'
+		}
+	},
+	condutores: [
+		{
+			nome: 'Daniel',
+			idade: 32
+		},
+		{
+			nome: 'Flávia',
+			idade: 25
+		}
+	],
+	calcularValorDoSeguro: {}
+};
+
+console.log(carro);
+
+//* Formas de se criar Objetos
+
+// 1 - Literal
+
+const obj1 = {};
+console.log(obj1);
+
+// 2 - Nativa
+
+const obj2 = new Object();
+console.log(obj2);
+
+// 3 - Funções construtoras
+
+function Produto(nome, preco, desc) {
+	this.nome = nome;
+	this.getPrecoComDesconto = () => {
+		return preco * (1 - desc);
+	};
+}
+
+const prod1 = new Produto('Café', 12.69, 0.15);
+
+console.log(prod1.getPrecoComDesconto().toFixed(2));
+
+// 4 - Função Factory
+
+function Funcionario(nome, salarioBase, faltas) {
+	return {
+		nome,
+		salarioBase,
+		faltas,
+		getSalario() {
+			return salarioBase / 30 * (30 - faltas);
+		}
+	};
+}
+
+const f1 = Funcionario('Daniel', 6500, 2);
+console.log(f1.getSalario().toFixed(2));
